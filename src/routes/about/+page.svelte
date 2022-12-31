48505cf778e5a2def6e0a226d8e257c0a6a6321e
tonaise <object1985@gmail.com>
2022-12-31 22:46:08 +0900

    fix #3 lesson04-TheNetNinja

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,21 @@
+<div class="about">
+	<h2>About Us</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus atque
+		sed porro assumenda magnam! At, ipsum reprehenderit! Repellat,
+		obcaecati! Laudantium nihil laborum amet incidunt consequatur ipsam,
+		ipsa molestiae reprehenderit exercitationem minima optio, tempora
+		officia, quam eaque deserunt. Delectus sed harum assumenda asperiores
+		provident, enim ab laudantium dicta vel minus repellat doloremque modi
+		quaerat amet ipsam! Iusto corporis nisi pariatur dicta.
+	</p>
+	<a href="/">back to top</a>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
